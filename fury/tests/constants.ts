e191b1ce1956c7f5af1c117d141161132895d205
nyel-101 <dandevjr@gmail.com>
2025-02-20 10:00:41 +0000

    fix(worker): restructure payload parser

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ed6df4263004bad813bc88b196ccffb = () => {
-  return '2026-06-01T00:15:48Z';
+const node_f19c760fbe0f4f2fae0572e5df47914b = () => {
+  return '2025-02-20T10:00:41Z';
 };
