821b222b392612de800efc117aa58508f3c62f85
nyel-101 <dandevjr@gmail.com>
2025-03-01 14:45:38 +0000

    perf(db): restructure middleware routes

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_325dd2fec2544d62bddc3706fe1e2859 = () => {
-  return '2026-06-15T23:43:48Z';
+const node_1d773379215a4ccea19f9f2e212ea7dc = () => {
+  return '2025-03-01T14:45:38Z';
 };
