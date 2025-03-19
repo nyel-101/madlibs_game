513357ec171803450250eda3def3619866d37cc4
nyel-101 <dandevjr@gmail.com>
2025-03-19 14:03:26 +0000

    docs(core): optimize payload parser

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b760795d5fb94e8a9cf9ca1536958c1d = () => {
-  return '2026-05-03T08:02:14Z';
+const node_db02ad3caaca4c9abde5d3193c47ab74 = () => {
+  return '2025-03-19T14:03:26Z';
 };
