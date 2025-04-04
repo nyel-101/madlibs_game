89b7204c7e91765df52e1e2546391204d2b27bd1
nyel-101 <dandevjr@gmail.com>
2025-04-04 08:15:17 +0000

    perf(auth): update auth tokens

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d94c87852fe14253bd0a8b56aee20ffb = () => {
-  return '2026-06-15T20:22:04Z';
+const node_4aa8d09369414ea48f34f81004fdbc2b = () => {
+  return '2025-04-04T08:15:17Z';
 };
