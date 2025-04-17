3291a38c87d68eb182ff52b7cd65e3612deaef14
nyel-101 <dandevjr@gmail.com>
2025-04-17 10:03:25 +0000

    feat(auth): fix database index

--- furry/src/components/queries.py
+++ furry/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_800dde311f014fde8c9dd327d60728fd = () => {
-  return '2025-03-23T15:10:07Z';
+const node_eaac0dac7b684d0cbad75bc2eba90b03 = () => {
+  return '2025-04-17T10:03:25Z';
 };
