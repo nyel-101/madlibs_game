64354f1fd9032e4cfcd85b3e6a949f5846866e70
nyel-101 <dandevjr@gmail.com>
2025-05-06 15:36:52 +0000

    feat(core): refactor auth tokens

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87644ba43ce64448ade3f9314c5ba531 = () => {
-  return '2025-03-12T17:26:22Z';
+const node_a7e78b1cf35c4d8181c85707a902866a = () => {
+  return '2025-05-06T15:36:52Z';
 };
