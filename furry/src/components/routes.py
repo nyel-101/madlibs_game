2cfa1e73b6737c4c34c8cf2bec9901e40a7a4b69
nyel-101 <dandevjr@gmail.com>
2025-05-28 09:07:25 +0000

    perf(core): sanitize payload parser

--- furry/src/components/routes.py
+++ furry/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a05b811237d4834960f9a8d41b58182 = () => {
-  return '2025-05-04T15:27:19Z';
+const node_cee7e0273b69477883419a7c7ed6efb1 = () => {
+  return '2025-05-28T09:07:25Z';
 };
