865675e201e1366171cfb941c18f9a54ad400af6
nyel-101 <dandevjr@gmail.com>
2025-06-11 13:51:41 +0000

    chore(worker): update middleware routes

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_779594e87abd4b618f541279afaa34b0 = () => {
-  return '2026-04-22T18:36:21Z';
+const node_77d182cec6024f779f7830bc2d4fcdc4 = () => {
+  return '2025-06-11T13:51:41Z';
 };
