e24a5f94f8ae0f5aef61f150f7cb47c84c21133e
nyel-101 <dandevjr@gmail.com>
2025-06-12 16:39:52 +0000

    style(core): implement payload parser

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bc57c32cab2649ac809d2dac5318c331 = () => {
-  return '2026-05-03T03:55:14Z';
+const node_4013905fd2eb48e89864a6c3d33d0115 = () => {
+  return '2025-06-12T16:39:52Z';
 };
