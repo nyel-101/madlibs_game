494deafb41c27c73a38b4f936cf3da1501e1b67b
nyel-101 <dandevjr@gmail.com>
2026-01-09 13:00:40 +0000

    chore(worker): fix logging interface

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60823f8ec7704406af96a05b8708566a = () => {
-  return '2026-01-07T21:39:27Z';
+const node_c993d476889a40058992f3163e639578 = () => {
+  return '2026-01-09T13:00:40Z';
 };
