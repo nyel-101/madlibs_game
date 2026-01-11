7e396600d2428e66a214f55d94c446f1a2b8e6aa
nyel-101 <dandevjr@gmail.com>
2026-01-11 14:17:24 +0000

    feat(utils): sanitize auth tokens

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_28839f9ed7b54fe49437d646762ee583 = () => {
-  return '2026-01-07T11:38:23Z';
+const node_8e755d718d904af59ef8b697dfff4ca5 = () => {
+  return '2026-01-11T14:17:24Z';
 };
