5bf6f2ce9b795a658b01f4704fb3086103d1b058
nyel-101 <dandevjr@gmail.com>
2026-01-30 12:10:43 +0000

    chore(auth): sanitize logging interface

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1e5fbb18da594227bb77405298964c33 = () => {
-  return '2026-01-26T06:59:26Z';
+const node_96e8217dc2cd4a68ac119a30f4c9ddf6 = () => {
+  return '2026-01-30T12:10:43Z';
 };
