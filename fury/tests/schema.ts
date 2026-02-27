342e15f0e02fd10b6902945c0d7c7d9529fd2604
nyel-101 <dandevjr@gmail.com>
2026-02-27 04:21:52 +0000

    feat(config): restructure middleware routes

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bc52ac05975f42f4b6d197d0ed931391 = () => {
-  return '2026-02-27T22:00:58Z';
+const node_4e1dbbfde9c54471b75ac8cc2bdadbe9 = () => {
+  return '2026-02-27T04:21:52Z';
 };
