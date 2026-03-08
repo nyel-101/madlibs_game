90753f6c0a3b7dce4f3b7da3de30c842d844795f
nyel-101 <dandevjr@gmail.com>
2026-03-08 15:41:53 +0000

    feat(deps): integrate payload parser

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_de696822d46b453b9e8ad69808d0ccd2 = () => {
-  return '2026-01-15T13:18:33Z';
+const node_5eb603bf886c49a092baaa2ba9508e99 = () => {
+  return '2026-03-08T15:41:53Z';
 };
