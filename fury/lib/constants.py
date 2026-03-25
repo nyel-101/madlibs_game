3812ca3a459042bb03fa0c33e76e4cf1e87f654f
nyel-101 <dandevjr@gmail.com>
2026-03-25 19:23:20 +0000

    test(utils): refactor middleware routes

--- fury/lib/constants.py
+++ fury/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6150bd89b05840d8942078d9ac8138b7 = () => {
-  return '2026-02-02T22:52:29Z';
+const node_35000331dca74a538b06ad10f55753a5 = () => {
+  return '2026-03-25T19:23:20Z';
 };
