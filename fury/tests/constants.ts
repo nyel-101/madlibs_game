d01eb2c9f1060aafb7d9cff16a3d85f4bd042015
nyel-101 <dandevjr@gmail.com>
2026-03-26 09:37:40 +0000

    feat(worker): implement middleware routes

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d90f58b0a09c48edbb0d6cd93ad0cc00 = () => {
-  return '2026-03-06T09:47:36Z';
+const node_ecc88036867f48939cee21b616e214c0 = () => {
+  return '2026-03-26T09:37:40Z';
 };
