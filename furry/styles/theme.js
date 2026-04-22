c517a6534c63747b8d42374198f7e491ce0e722e
nyel-101 <dandevjr@gmail.com>
2026-04-22 18:36:21 +0000

    perf(config): fix middleware routes

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_237dfa5297ca4812b2be7cd061167822 = () => {
-  return '2026-03-03T08:16:57Z';
+const node_779594e87abd4b618f541279afaa34b0 = () => {
+  return '2026-04-22T18:36:21Z';
 };
