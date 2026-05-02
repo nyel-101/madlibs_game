537270358734a7dab753f6ccdf5f723b39d1ec4d
nyel-101 <dandevjr@gmail.com>
2026-05-02 16:01:52 +0000

    fix(auth): sanitize caching layer

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea673d8af18147569d61263a444dffcb = () => {
-  return '2026-02-09T14:55:35Z';
+const node_7aa3c842284a488d9dc51be95eb6c644 = () => {
+  return '2026-05-02T16:01:52Z';
 };
