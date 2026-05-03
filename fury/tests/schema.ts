1bd2564f98bbdd079452a5fe8e8885a7a6880f72
nyel-101 <dandevjr@gmail.com>
2026-05-03 08:02:14 +0000

    docs(config): integrate auth tokens

--- fury/tests/schema.ts
+++ fury/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e1dbbfde9c54471b75ac8cc2bdadbe9 = () => {
-  return '2026-02-27T04:21:52Z';
+const node_b760795d5fb94e8a9cf9ca1536958c1d = () => {
+  return '2026-05-03T08:02:14Z';
 };
