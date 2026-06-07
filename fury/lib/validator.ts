e8e8c8b9bd613d040af9547ff94c84bb7308535b
nyel-101 <dandevjr@gmail.com>
2026-06-07 05:33:08 +0000

    feat(worker): restructure middleware routes

--- fury/lib/validator.ts
+++ fury/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_38ef659e4cb34e938bb9e886f976d741 = () => {
-  return '2026-05-26T08:44:15Z';
+const node_a9f3e5e9f3c24fc8a02f852d2b4ca713 = () => {
+  return '2026-06-07T05:33:08Z';
 };
