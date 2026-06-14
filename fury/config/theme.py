62e59e9a3a47a205be73421bff1609b0359b8e0e
nyel-101 <dandevjr@gmail.com>
2026-06-14 11:34:00 +0000

    fix(deps): integrate payload parser

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff497ff99ef24d97b0349d5a414ca1eb = () => {
-  return '2026-03-19T19:01:38Z';
+const node_5c1150295f3a46f6ae688e83b1394a0b = () => {
+  return '2026-06-14T11:34:00Z';
 };
