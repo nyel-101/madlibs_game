ba1478a0f4fcef69e4f15efbfad8853505730ee5
nyel-101 <dandevjr@gmail.com>
2026-06-16 18:33:15 +0000

    test(config): optimize middleware routes

--- furry/config/constants.py
+++ furry/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_588a1bab7c58432f801c42d07ad4d54d = () => {
-  return '2026-04-20T19:45:57Z';
+const node_cc33de105fef45a682a860fed5b1610e = () => {
+  return '2026-06-16T18:33:15Z';
 };
