3c36e08275b47bab569e6b7c358fde02620e2d32
nyel-101 <dandevjr@gmail.com>
2026-06-16 08:23:58 +0000

    chore(utils): restructure caching layer

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ea79a5d4418840cb81fcffb6b02e2e8f = () => {
-  return '2026-03-02T08:40:34Z';
+const node_1e14fd77f03e49959f1ca61e9efc50b8 = () => {
+  return '2026-06-16T08:23:58Z';
 };
